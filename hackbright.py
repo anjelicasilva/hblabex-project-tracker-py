90b37e3064984af206047479e2bbca759f7ecfb4
Your Name <a.vo741@gmail.com>
2019-12-05 20:48:41 -0800

    Implement assign_grade function

--- hackbright.py
+++ hackbright.py
@@ -90,7 +90,16 @@ def get_grade_by_github_title(github, title):
 
 def assign_grade(github, title, grade):
     """Assign a student a grade on an assignment and print a confirmation."""
-    pass
+    QUERY = """
+        INSERT INTO grades (student_github, project_title, grade)
+        VALUES (:github, :title, :grade)
+        """
+
+    db.session.execute(QUERY, {'github': github, 'title': title, 'grade': grade})
+
+    db.session.commit()
+
+    print(f"Grade assigned for project {title} to {github}.")
 
 
 def handle_input():
